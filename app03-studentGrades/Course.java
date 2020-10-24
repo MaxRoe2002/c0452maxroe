1111c59335cf19a37ad96b9193bebe5ef9994d18
Max Roe <71944268+MaxRoe2002@users.noreply.github.com>
2020-10-24 19:56:01 +0100

    completed
    completed coding for courses and modules

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,35 +1,104 @@
 
 /**
- * Write a description of class Course here.
+ * Course class to create modules and grades.
  *
- * @author (Max ROe)
- * @version (06/10/20)
+ * @author (Max Roe)
+ * @version (2020/24/10)
  */
 public class Course
 {
     // course code
-    private String moduleNumber;
+    private String codeNo;
     //course title
     private String title;
+    //module names
+    private Module module1;
+    private Module module2;
+    private Module module3;
+    private Module module4;
+    //calculating final mark
+    private int finalMark;
+    private String finalGrade;
     
 
     /**
      * Constructor for objects of class Course
      */
-    public Course(String moduleNumber, String title)
+    public Course(String codeNo, String title)
     {
-        this.moduleNumber = moduleNumber;
+        this.codeNo = codeNo;
         this.title = title;
+        
+        this.finalMark = 0;
+        this.finalGrade = null;
+        
+        createModules();
     }
-
-    /**
-     * Method that returns title
-     * 
-     * @return title
-     */
-    public String getTitle()
+    
+    public void createModules()
     {
-        // return title
-        return title;
+        module1 = new Module("C0452", "Programming");
+        module2 = new Module("C0451", "Web Development");
+        module3 = new Module("C0450", "Computer Architecture");
+        module4 = new Module("C0453", "Digital Technologies");
+    }
+    
+    public void addModule(Module module, int moduleNo)
+    {
+        if(moduleNo == 1)
+        {
+            this.module1 = module;
+        }
+    }
+    
+    public void setMark(int mark, String codeNo)
+    {
+        if(module1.getCodeNo() == codeNo)
+        {
+            module1.awardMark(mark);
+        }
+    }
+    
+    public void calculateFinalMark()
+    {
+        if(courseCompleted())
+        {
+        int totalMark = module1.getMark() + module2.getMark() +
+        module3.getMark() + module4.getMark();
+        
+        finalMark = totalMark / 4;
+        print();
+       } 
+       else
+       {
+           
+       }
+    }
+    
+    public boolean courseCompleted()
+    {
+        if((module1.isCompleted()) && (module2.isCompleted()))
+        {
+            return true;
+        }
+        else return false;
+    }
+    
+    /**
+     * Prints out the details of a course
+     */
+    public void print()
+    {
+        System.out.println("Course " + codeNo + " - " + title);
+    }
+    
+    public void printModules()
+    {
+        //print all the individual marks 
+        if(courseCompleted())
+       {
+    
+        System.out.println("Final Mark = " + finalMark);
+       }
     }
 }
--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Module here.
+ * Class to create modules and awarding student with marks.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Max Roe)
+ * @version (2020.24.10)
  */
 public class Module
 {
@@ -14,6 +14,12 @@ private String codeNo;
 
 private int mark;
 
+private int credit;
+
+private boolean completed;
+
+private boolean passed;
+
 /**
      * Constructor for objects of class Module
      */
@@ -21,7 +27,51 @@ private int mark;
     {
         this.title = title;
         this.codeNo = codeNo;
+        
+        mark = -1;
+        credit = 15;
+        
+        passed = false;
+        completed = false;
     }
     
+    /**
+     * Awarding a mark
+     */
+    public void awardMark(int mark)
+   { 
+       this.mark = mark;
+   } 
+   
+   /**
+    * Returns the module mark
+    */
+   public int getMark()
+   {
+       return mark;
+   }
+   
+   /**
+    * Returns the module code number
+    */
+   public String getCodeNo()
+   {
+       return codeNo;
+   }
     
+   /**
+    * Boolean to return a mark
+    */
+   public boolean isCompleted()
+   {
+       return mark >= 0;
+   }
+   
+   /**
+    * Print module title and mark
+    */
+   public void print()
+   {
+      System.out.println("Module: " + title + " " + codeNo + " Mark = " + mark);
+   }
 }
