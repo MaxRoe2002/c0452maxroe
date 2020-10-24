0e7d9d13ce5454d079d93347abc0c5279ea2ba62
Max Roe <71944268+MaxRoe2002@users.noreply.github.com>
2020-10-24 18:08:25 +0100

    modules
    created modiule class

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -7,12 +7,21 @@
  */
 public class Module
 {
+// Fields
+private String title;
 
-    /**
+private String codeNo;
+
+private int mark;
+
+/**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String title, String codeNo)
     {
+        this.title = title;
+        this.codeNo = codeNo;
     }
+    
     
 }
